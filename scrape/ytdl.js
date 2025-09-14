fd32e670bd87bc2376b243c5141fd35d246aa2e3
ShotaQuen <imanadja86@gmail.com>
2025-09-14 17:46:36 +0800

    update ytdl.js

--- scrape/ytdl.js
+++ scrape/ytdl.js
@@ -1,5 +1,34 @@
 const axios = require('axios')
 
+const RAPID_KEY = "1dda0d29d3mshc5f2aacec619c44p16f219jsn99a62a516f98"; // ganti dengan key kamu
+
+async function fetchMedia(url) {
+  if (!url || !url.includes("https://")) throw new Error("Url is required");
+
+  const { data } = await axios.post(
+    "https://auto-download-all-in-one.p.rapidapi.com/v1/social/autolink",
+    { url },
+    {
+      headers: {
+        "accept-encoding": "gzip",
+        "cache-control": "no-cache",
+        "content-type": "application/json; charset=utf-8",
+        referer: "https://auto-download-all-in-one.p.rapidapi.com/",
+        "user-agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36",
+        "x-rapidapi-host": "auto-download-all-in-one.p.rapidapi.com",
+        "x-rapidapi-key": RAPID_KEY,
+      },
+    }
+  );
+
+  if (data.error) {
+    throw new Error("Download failed");
+  }
+
+  return data;
+}
+
 async function ytmp3(url) {
   try {
     const data = await fetchMedia(url);
