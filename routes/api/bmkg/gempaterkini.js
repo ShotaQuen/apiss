363a7c9dfecb755c3660eb85494e02ebe5f66352
Shota Quen <imanadja86@gmail.com>
2025-10-06 19:45:56 +0800

    update gempaterkini.js

---
+++ routes/api/bmkg/gempaterkini.js
@@ -0,0 +1,30 @@
+async function bmkg() {
+  try {
+    const res = await axios.get(`https://data.bmkg.go.id/DataMKG/TEWS/gempaterkini.json`)
+    return res.data
+  } catch (err) {
+    console.error(err.message)
+    return null
+  }
+}
+
+router.get("/gempaterkini", async (req, res) => {
+  try {
+    const result = await bmkg();
+
+    res.json({
+      status: true,
+      creator: "BerakNews",
+      result
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: false,
+      error: error
+    });
+  }
+});
+
+router.example_response = "";
+module.exports = router;
