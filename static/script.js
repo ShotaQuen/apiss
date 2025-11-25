1d3cdaab1975c5f22d57b69e36cb15f3e2f895aa
Shota Quen <imanadja86@gmail.com>
2025-11-25 18:27:40 +0800

    update script.js

--- static/script.js
+++ static/script.js
@@ -108,7 +108,7 @@ function updateActiveNavLink() {
 async function loadApiDocumentation() {
     try {
         showLoading();
-        const response = await fetch('https://berak-new-pjq3.vercel.app/api/clean');
+        const response = await fetch('https://berak-new-pjq3.vercel.app/api/check');
         apiData = await response.json();
         
         if (apiData.status) {
